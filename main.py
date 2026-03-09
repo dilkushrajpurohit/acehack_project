4811c46d62238c004768d1d2596329124ec8fd1f
HarshSogra <harshsogra@gmail.com>
2026-03-09 16:21:12 +0530

    Temporary allow ALL origins

--- main.py
+++ main.py
@@ -37,8 +37,7 @@ if extra_origins:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
-    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
